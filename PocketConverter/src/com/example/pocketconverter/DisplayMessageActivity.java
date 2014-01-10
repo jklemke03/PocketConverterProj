3233b3e1e0de7766c973a4a8062e27243c21bc12
Curran Hamilton <curranbhamilton@gmail.com>
2014-01-09 18:04:44 -0800

    Added activity to manifest Added launch icon Added settings icon Reformatted text fields Changed text of Button

--- PocketConverter/src/com/example/pocketconverter/DisplayMessageActivity.java
+++ PocketConverter/src/com/example/pocketconverter/DisplayMessageActivity.java
@@ -11,25 +11,28 @@ import android.annotation.TargetApi;
 import android.content.Intent;
 import android.os.Build;
 
-@SuppressLint("NewApi")
 public class DisplayMessageActivity extends Activity {
     @SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_display_message);
+		
 		//Make sure we're running on Honeycomb or Higher to use ActionBar APIs
 		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB){
 			//Show the Up button in the action bar.
 			getActionBar().setDisplayHomeAsUpEnabled(true);
 		}
+		
 		//Calls Intent to receive the data 
 		Intent intent = getIntent();
 		String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+		
 		//Create the text view
 		TextView textView = new TextView(this);
 		textView.setTextSize(40);
 		textView.setText(message);
+		
 		//Set the text view a the activity layout
 		setContentView(textView);
 	}
@@ -43,6 +46,7 @@ public class DisplayMessageActivity extends Activity {
 			getActionBar().setDisplayHomeAsUpEnabled(true);
 		}
 	}
+	
 	/**
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
--- PocketConverter/src/com/example/pocketconverter/MainActivity.java
+++ PocketConverter/src/com/example/pocketconverter/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.pocketconverter;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.content.Intent;
 import android.widget.EditText;
@@ -28,19 +29,39 @@ public class MainActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
+		
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
     //Called when the user clicks the Send Button
-	public void sendMessage(View view){
+	public void convertNumber(View view) {
+		
 		//Creates an intent object used to start another activity
 		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		
 		//Finds the text value for decimal textfield
 		EditText editText = (EditText) findViewById(R.id.dec_field);
 		String message = editText.getText().toString();
 		intent.putExtra(EXTRA_MESSAGE, message);
 		startActivity(intent);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        case R.id.action_settings:
+	            openSettings();
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+
+	private void openSettings() {
+		// TODO create method for opening the setting page.
+		
 	}
 	
 }
