9bfaa5474cfcb165f3482eb5693ab7c049e34b46
jklemke03 <jklemke03@gmail.com>
2014-01-19 16:14:29 -0800

    Created Working Reset Button

--- PocketConverter/src/com/example/pocketconverter/MainActivity.java
+++ PocketConverter/src/com/example/pocketconverter/MainActivity.java
@@ -21,6 +21,15 @@ import android.widget.EditText;
 
 public class MainActivity extends Activity {
 	public final static String EXTRA_MESSAGE = "com.example.pocketconverter.MESSAGE";
+	private EditText decimalText;
+	private EditText octText;
+	private EditText hexText;
+	private EditText binText;
+	private String binInput;
+	private String decInput;
+	private String hexInput;
+	private String octInput;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -43,34 +52,34 @@ public class MainActivity extends Activity {
 		String decToOct;
 		
 		//Finds the text value for decimal text field
-		EditText decimalText = (EditText) findViewById(R.id.dec_field);
-		String decInput = decimalText.getText().toString();
+	    decimalText = (EditText) findViewById(R.id.dec_field);
+		decInput = decimalText.getText().toString();
 		int decimalNumber = 0;
 		
 		//Test the input of the decimal field
 		final StringBuilder errorMessage = new StringBuilder("Bad Input Value: ");
-		try {
-			decimalNumber = Integer.parseInt(decInput);
-		} catch (final NumberFormatException e) {
-			errorMessage.append("Failed to parse decimal number" + e.getMessage());
-		}
 				
 		//Finds text value for binary text field
-		EditText binText = (EditText) findViewById(R.id.bin_field);
-		String binInput = binText.getText().toString();
+		binText = (EditText) findViewById(R.id.bin_field);
+		binInput = binText.getText().toString();
 		
 		//Finds text value for hexadecimal text field
-		EditText hexText = (EditText) findViewById(R.id.hex_field);
-		String hexInput = hexText.getText().toString();
+		hexText = (EditText) findViewById(R.id.hex_field);
+		hexInput = hexText.getText().toString();
 		
 		//Finds text for value for octal text field
-		EditText octText = (EditText) findViewById(R.id.oct_field);
-		String octInput = octText.getText().toString();
+		octText = (EditText) findViewById(R.id.oct_field);
+		octInput = octText.getText().toString();
 		
 		//Find which text field is not empty in order to create intent
 		//May be simpler way to do this. Also need to check if fields empty or more than one filled
 		
 		if(!decInput.equals("")){
+			try {
+				decimalNumber = Integer.parseInt(decInput);
+			} catch (final NumberFormatException e) {
+				errorMessage.append("Failed to parse decimal number" + e.getMessage());
+			}
 			//Call method that will convert the other forms
 			decToHex = calc.convertHex(decimalNumber);
 			decToBin = calc.convertBin(decimalNumber);
@@ -109,11 +118,25 @@ public class MainActivity extends Activity {
 			binText.setText(decToBin);
 			hexText.setText(decToHex);
 		}
-		else
-		{
-			//none have anything in them. Perhaps message inputed onto screen?
-		}
 	
+	}
+	
+	public void resetNumber(View view){
+		
+		String emptyString = "";
+		this.decInput = "";
+		this.binInput = "";
+		this.hexInput = "";
+		this.octInput = "";
+		binText = (EditText) findViewById(R.id.bin_field);
+		hexText = (EditText) findViewById(R.id.hex_field);
+		octText = (EditText) findViewById(R.id.oct_field);
+		decimalText = (EditText) findViewById(R.id.dec_field);
+		this.binText.setText(emptyString);
+		this.hexText.setText(emptyString);
+		this.octText.setText(emptyString);
+		this.decimalText.setText(emptyString);
+		
 	}
 	
 
